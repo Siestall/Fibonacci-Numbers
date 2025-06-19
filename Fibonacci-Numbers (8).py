396c016696be00dfeffea7a0035535a7010217c2
Andrey <andrey.nevedskiy@mail.ru>
2025-06-19 08:48:10 +0300

    Add files via upload

---
+++ Fibonacci-Numbers (8).py
@@ -0,0 +1,20 @@
+#Fibonacci-Numbers (8)
+try:
+    len_nums = int(input('Enter length: '))
+    if len_nums > 2:
+        num1 = 1
+        num2 = 1
+        res = ''
+        print('1,1',end=',')
+        count = 0
+        while count < len_nums - 2:
+            sum_fib = num1 + num2
+            num1 = num2
+            num2 = sum_fib
+            count+=1
+            res += str(sum_fib) + ','
+        print(res[:-1])
+    else:
+        print('len_nums > 2')
+except:
+    print('Enter integer')
